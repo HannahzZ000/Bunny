deacaa4adff7af48c83c945b6b9cceab77812714
HannahzZ000 <yunbing2001@gmail.com>
2025-01-25 12:40:10 -0500

    Initial Unity project upload

---
+++ Assets/script/SceneTransition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    // 设置要跳转的场景名称
+    public string targetSceneName = "ARScene";
+
+    void Update()
+    {
+        // 检测屏幕点击事件
+        if (Input.GetMouseButtonDown(0)) // 左键鼠标点击或手机屏幕点击
+        {
+            // 加载目标场景
+            LoadARScene();
+        }
+    }
+
+    // 加载场景的方法
+    void LoadARScene()
+    {
+        SceneManager.LoadScene(targetSceneName);
+    }
+}
